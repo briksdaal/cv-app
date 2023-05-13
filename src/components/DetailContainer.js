8647f25e815c571f6d899df355f30d1ba7bf97c4
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-05-14 02:42:23 +0300

    Refactor Details to use mapping and container

---
+++ src/components/DetailContainer.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import { FaRegEnvelope } from 'react-icons/fa';
+import { BsTelephoneFill, BsGlobe2 } from 'react-icons/bs';
+import { IoLocationSharp } from 'react-icons/io5';
+
+import EditableContent from './EditableContent';
+import { handleChildUpdates } from './helpers/helperFunctions';
+
+export default class DetailContainer extends Component {
+  render() {
+    const iconTypes = {
+      email: <FaRegEnvelope />,
+      phone: <BsTelephoneFill />,
+      address: <IoLocationSharp />,
+      url: <BsGlobe2 />,
+    };
+
+    const { children, icon } = this.props;
+    return (
+      <div className="detail-container">
+        <i className="detail-icon">
+          {iconTypes[icon]}
+        </i>
+        {children}
+      </div>
+    );
+  }
+}
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import EditableContent from './EditableContent';
+import DetailContainer from './DetailContainer';
 import { handleChildUpdates } from './helpers/helperFunctions';
 
 export default class Details extends Component {
@@ -7,38 +8,20 @@ export default class Details extends Component {
     const { details, updateDetails, changeCurrentEdits } = this.props;
     return (
       <>
-        <EditableContent
-          text={details.email}
-          className="details-email"
-          handleGlobalStateUpdate={(newChild) => handleChildUpdates(details, newChild, 'email', updateDetails)}
-          changeCurrentEdits={changeCurrentEdits}
-        >
-          {details.email}
-        </EditableContent>
-        <EditableContent
-          text={details.phone}
-          className="details-phone"
-          handleGlobalStateUpdate={(newChild) => handleChildUpdates(details, newChild, 'phone', updateDetails)}
-          changeCurrentEdits={changeCurrentEdits}
-        >
-          {details.phone}
-        </EditableContent>
-        <EditableContent
-          text={details.address}
-          className="details-address"
-          handleGlobalStateUpdate={(newChild) => handleChildUpdates(details, newChild, 'address', updateDetails)}
-          changeCurrentEdits={changeCurrentEdits}
-        >
-          {details.address}
-        </EditableContent>
-        <EditableContent
-          text={details.url}
-          className="details-url"
-          handleGlobalStateUpdate={(newChild) => handleChildUpdates(details, newChild, 'url', updateDetails)}
-          changeCurrentEdits={changeCurrentEdits}
-        >
-          {details.url}
-        </EditableContent>
+        {Object.entries(details).map((detail) => (
+          <DetailContainer icon={detail[0]} key={detail[0]}>
+            <EditableContent
+              text={detail[1]}
+              className={`details-${detail[0]}`}
+              handleGlobalStateUpdate={
+                (newChild) => handleChildUpdates(details, newChild, detail[0], updateDetails)
+              }
+              changeCurrentEdits={changeCurrentEdits}
+            >
+              {detail[1]}
+            </EditableContent>
+          </DetailContainer>
+        ))}
       </>
     );
   }
