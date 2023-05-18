f2cbcf7ea36cdd028fb47bead9768219092e82ad
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-05-18 22:55:48 +0300

    Move data formatting to App and pass as prop to CV

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import uniqid from 'uniqid';
+import { getDataWithKeys } from './components/helpers/helperFunctions';
+import { defaultDataRaw, dummyDataRaw } from './components/defaultData';
+
 import CV from './components/CV';
 
 export default function App() {
-  return <CV />;
+  const defaultData = getDataWithKeys(defaultDataRaw, uniqid);
+  const dummyData = getDataWithKeys(dummyDataRaw, uniqid);
+  return (
+    <CV
+      getNextId={uniqid}
+      defaultData={defaultData}
+      dummyData={dummyData}
+    />
+  );
 }
